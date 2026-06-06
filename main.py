354d54598f49b180bf3edc408ccdbd1866810d75
Оразлиев Шамиль <orazlievshamil@gmail.com>
2026-06-07 00:25:11 +0300

    Complete 3rd tests

--- main.py
+++ main.py
@@ -44,39 +44,51 @@ def create_book(new_book:New_Book):
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
+#----------------------------------------------------------------------------------------
+from fastapi import FastAPI
+from pydantic import BaseModel, Field, EmailStr, ConfigDict
+#BaseModel — это базовый класс, от которого наследуются все модели данных в Pydantic.
+#Field - это функция, которая используется для добавления метаданных и дополнительных правил валидации к полю модели
+# Для более строгой валидации данных
+#EmailStr - это подкласс str (строки) с дополнительной, строгой валидацией формата email
+data_wo_age = {
+    "email":"abc@mail.ru",
+    "bio":None,
+    "brthd":"2022",#1)При запуске с этими 2 доп-ми параметрами и без обозначения их в классе UserSChema ошибки не будет
+    "gender":"male"#При передаче данных в Pyd-c с полями, которые не определены в схеме, они просто игнорируются(особенность Py-c)
+}
+data = {
+    "email": "abc@mail.ru",
+    "bio": None,
+    "age":12
+}
+class UserSChema(BaseModel):
+    email:EmailStr
+    bio:str | None = Field(max_length=100)
 
+#1) Но иногда нам надо что эти избыточные данные не игнорировались
+#Для этого можно использовать ConfigDict (не разрешает доп-ые input-ы)
+    model_config = ConfigDict(extra="forbid")
 
-
-
-
-
-
-# blacklisted = ["удача"] #input("Навыки, которые находятся в чёрном списке: ")
-# preferred = ["атака", "защита"] #input("Навыки которые особенно важны: ")
-# options = (("атака",20),("защита",20),("скорость",10))
-#
-# def skills(options,preferred,blacklisted):
-#     preferred_q = []
-#     neutral = []
-#     count_black = 0
-#     count_pref = 0
-#
-#     for i in range(0,3):
-#         if options[i][0] in blacklisted:
-#             count_black+=1
-#             if count_black == 3:
-#                 return 20
-#             continue
-#         elif options[i][0] in preferred:
-#             count_pref+=1
-#             preferred_q.append(options[i])
-#         else:
-#             neutral.append(options[i])
-#
-#     if count_pref == 0:
-#         return max(neutral, key = lambda x: x[1])
-#
-#     else:
-#         return max(preferred_q, key = lambda x: x[1])
-#
-# print(skills(options,preferred,blacklisted))
+class UserAgeSchema(UserSChema):
+    age:int = Field(ge=0, le=120)
+print(repr(UserSChema(**data_wo_age)))#repr - для более немного информативного вывода
+print(repr(UserAgeSchema(**data)))
+#-----------------------------------------------------------------------------
+from pydantic import BaseModel, Field, EmailStr
+from fastapi import FastAPI
+app = FastAPI()
+users = []
+data_wo_age = {
+    "email":"abc@mail.ru",
+    "bio":None
+}
+class UserSChema(BaseModel):
+    email:EmailStr
+    bio:str | None = Field(max_length=100)
+@app.get("/users")
+def get_users():
+    return users
+@app.post("/users")
+def add_users(user:UserSChema):
+    users.append(user)
